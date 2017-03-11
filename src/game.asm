1f4d92b53d397688e1a555bc75faa0cceaa44e1a
eonarheim <erik.onarheim@gmail.com>
2017-03-11 00:37:02 -0600

    Update with new sprites

--- src/game.asm
+++ src/game.asm
@@ -272,10 +272,10 @@ palette:
 
 sprites:
      ;vert tile attr horiz
-  .db $80, $32, $00, $80   ;sprite 0
-  .db $80, $33, $00, $88   ;sprite 1
-  .db $88, $34, $00, $80   ;sprite 2
-  .db $88, $35, $00, $88   ;sprite 3
+  .db $80, $00, $00, $80   ;sprite 0
+  .db $80, $01, $00, $88   ;sprite 1
+  .db $88, $10, $00, $80   ;sprite 2
+  .db $88, $11, $00, $88   ;sprite 3
 
 
 ;;;;;;;;;;;;;;  
@@ -303,4 +303,4 @@ nescallback:
   
   .bank 2
   .org $0000
-  .incbin "mario.chr"   ;includes 8KB graphics file from SMB1
+  .incbin "player.chr"   ;includes 8KB graphics file from SMB1
